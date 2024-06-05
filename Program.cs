1cb2b165debe66cc12d6ce95d819a1285f044bbc
Izzatillo Abdusamatov <abdusamatovtillo8@gmail.com>
2024-06-05 12:31:09 +0500

    O'zgaruvchilar

--- Program.cs
+++ Program.cs
@@ -1,7 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿﻿using System;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("salom");
 
 
-int FirstNumber = 26;
-int SecondNumber = 22;
 
+// O`zgaruvchilar!!
+
+int firstNumber = 5;
+int secondNumber = 10;
+bool rost = true;
+bool yolgon = false;
+double thirdNumber = 2.7;
+double fourthNumber = 12.5;
+
+int qoshish = firstNumber + secondNumber;
+Console.WriteLine("Natija = " + qoshish);
+
+int ayirish = firstNumber - secondNumber;
+Console.WriteLine("Natija = " + ayirish);
+
+int kopaytirish = firstNumber * secondNumber;
+Console.WriteLine("Natija = " + kopaytirish);
+
+int bolish = firstNumber / secondNumber;
+Console.WriteLine("Natija = " + bolish);
+
+bool booleanFirst = firstNumber < thirdNumber;
+Console.WriteLine("Natija = " + booleanFirst);
+
+bool booleanSecond = fourthNumber > secondNumber;
+Console.WriteLine("Natija =" + booleanSecond);
+
+bool booleanThird = (rost=yolgon);
+Console.WriteLine("Natija = " + booleanThird);
+
+double doubleFirst = thirdNumber + fourthNumber;
+Console.WriteLine("Natija = " + doubleFirst);
+
+double doubleSecond = firstNumber - fourthNumber;
+Console.WriteLine("Natija = " + doubleSecond);
